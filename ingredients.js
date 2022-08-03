ada4737df1c7b03cc610202f94010666813c37f6
SimRai32 <kaliraisimran@gmail.com>
2022-08-03 00:53:41 +0000

    ingredients.js for loop

---
+++ ingredients.js
@@ -0,0 +1,5 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+  for (let i = 0; i < ingredients.length; i++){
+    console.log(ingredients[i]);
+  }
