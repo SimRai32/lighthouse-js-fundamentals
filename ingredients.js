cb4cf2d3a35fcb962e1c8500a4279b4fb5dd561a
SimRai32 <kaliraisimran@gmail.com>
2022-08-03 00:56:37 +0000

    ingredients.js reverse loop

--- ingredients.js
+++ ingredients.js
@@ -1,8 +1,5 @@
 const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
 
-let i = 0;
-
-while(i < ingredients.length){
+for (let i = ingredients.length - 1; i >= 0; i--){
   console.log(ingredients[i]);
-  i++;
 }
